a41db775158fd9444281de7abd96ea56140711c6
DanielSnorkelss <10874707@iesmarxadella.org>
2026-01-30 18:21:29 +0100

    Add files via upload

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"U4","c":"analisisNotas","l":"analisisNotas()","u":"%3Cinit%3E()"},{"p":"U4","c":"analisisNotas","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"U4","l":"analisisNotas"}];updateSearchResults();
